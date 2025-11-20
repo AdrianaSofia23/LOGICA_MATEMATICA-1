a0a8cdbb775199247bbeeb4bf877677ae96d2f5d
AdrianaSofia23 <106442160+AdrianaSofia23@users.noreply.github.com>
2025-11-20 10:10:16 -0500

    Add grammar rules for a simple parser

---
+++ lab09/grammar.pl
@@ -0,0 +1,37 @@
+determiner --> [the] | [a] | [an].
+
+noun --> [cat] | [dog] | [bird] | [fish]
+       | [teacher] | [student] | [friend]
+       | [apple] | [car] | [house] | [computer].
+
+
+personal_pronoun --> [i] | [you] | [he] | [she] | [it] | [we] | [they].
+
+
+adjective --> [big] | [small] | [angry] | [beautiful].
+adjectives --> [] | adjective, adjectives.
+
+adverb --> [quickly] | [slowly] | [happily] | [sadly] | [silently] | [loudly].
+
+verb_present --> [eat] | [eats] | [see] | [sees]
+               | [run] | [runs] | [drive] | [drives]
+               | [study] | [studies] | [play] | [plays].
+
+verb_past --> [ate] | [saw] | [ran] | [drove] | [studied] | [played].
+
+base_verb --> [eat] | [see] | [run] | [drive] | [study] | [play].
+
+verb_future --> [will], base_verb.
+
+verb_any --> verb_present.
+verb_any --> verb_past.
+verb_any --> verb_future.
+
+sentence --> noun_phrase, verb_phrase.
+
+noun_phrase --> personal_pronoun.
+noun_phrase --> determiner, adjectives, noun.
+
+verb_phrase --> verb_any, noun_phrase.
+verb_phrase --> adverb, verb_any, noun_phrase.
+verb_phrase --> verb_any, adverb, noun_phrase.
